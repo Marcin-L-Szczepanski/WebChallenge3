4dff0ca50856c3cd10b1fe53052fec68f1df959f
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-09-02 15:58:51 +0900

    Style menu for mobile and add action to the menu button to show menu

--- src/blocks/button/--menu/button--menu.js
+++ src/blocks/button/--menu/button--menu.js
@@ -2,4 +2,5 @@ var button = document.querySelector(".button--menu");
 button.addEventListener("click", function () {
     document.querySelector(".button--menu").classList.toggle("button--close");
     document.querySelector(".button--menu").classList.toggle("button--open");
+    document.querySelector(".menu__list").classList.toggle("menu__list--open");
 }, false);
