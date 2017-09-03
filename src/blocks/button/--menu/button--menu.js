739a8f75081ffea17e2b88a0a4c87de3e9ff154a
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-09-03 09:34:57 +0900

    Close open menu after clicking menu item

--- src/blocks/button/--menu/button--menu.js
+++ src/blocks/button/--menu/button--menu.js
@@ -1,6 +1,16 @@
 var button = document.querySelector(".button--menu");
 button.addEventListener("click", function () {
+    "use strict";
     document.querySelector(".button--menu").classList.toggle("button--close");
     document.querySelector(".button--menu").classList.toggle("button--open");
     document.querySelector(".menu__list").classList.toggle("menu__list--open");
 }, false);
+
+var link = document.querySelectorAll(".menu__item");
+for (var i=0; i<link.length; i++) {
+link[i].addEventListener("click", function () {
+    document.querySelector(".button--menu").classList.toggle("button--close");
+    document.querySelector(".button--menu").classList.toggle("button--open");
+    document.querySelector(".menu__list").classList.toggle("menu__list--open");
+}, false);
+}
