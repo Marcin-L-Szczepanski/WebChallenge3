73fcb6a7780d224389ccb8683a1aaf6d3b99a762
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-09-03 10:36:17 +0900

    Add togle function for button and close function for menu items

--- src/blocks/button/--menu/button--menu.js
+++ src/blocks/button/--menu/button--menu.js
@@ -1,14 +1,37 @@
-function toggleMenu() {
+function toggleButtonIcon() {
     "use strict";
     document.querySelector(".button--menu").classList.toggle("button--close");
     document.querySelector(".button--menu").classList.toggle("button--open");
+}
+
+function toggleShowMenu() {
+    "use strict";
     document.querySelector(".menu__list").classList.toggle("menu__list--open");
 }
 
-var button = document.querySelector(".button--menu");
-button.addEventListener("click", toggleMenu);
+function closeButtonIcon() {
+    "use strict";
+    document.querySelector(".button--menu").classList.add("button--close");
+    document.querySelector(".button--menu").classList.remove("button--open");
+}
 
+function closeShowMenu() {
+    "use strict";
+    document.querySelector(".menu__list").classList.remove("menu__list--open");
+}
+
+var button = document.querySelector(".button--menu");
+button.addEventListener("click", function () {
+    "use strict";
+    toggleButtonIcon();
+    toggleShowMenu();
+});
+
+var list = document.querySelector(".menu__list");
 var link = document.querySelectorAll(".menu__item");
-for (var i=0; i<link.length; i++) {
-    link[i].addEventListener("click", toggleMenu);
+for (var i=0; i<=link.length; i++) {
+    link[i].addEventListener("click", function(){
+        closeButtonIcon();
+        closeShowMenu();
+    });
 }
