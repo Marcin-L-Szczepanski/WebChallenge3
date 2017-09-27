037f8f213de28e3474d0ba31fd5d2452bec168d6
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-09-28 07:30:04 +0900

    Remove menu list when resizing window

--- src/blocks/menu/__item/menu__item.js
+++ src/blocks/menu/__item/menu__item.js
@@ -7,8 +7,17 @@ function closeButtonIcon() {
 function closeShowMenu() {
     "use strict";
     document.querySelector(".menu__list").classList.remove("menu__list--open");
-    document.querySelector("body").classList.toggle("body-cover");
+    document.querySelector("body").classList.remove("body-cover");
 }
+
+window.addEventListener("resize", function () {
+    "use strict";
+    var screenWidth = document.body.scrollWidth;
+    if (screenWidth > 633) {
+        closeButtonIcon();
+        closeShowMenu();
+    }
+});
 
 var list = document.querySelector(".menu__list");
 var link = document.querySelectorAll(".menu__item");
@@ -18,3 +27,5 @@ for (var i=0; i<=link.length; i++) {
         closeShowMenu();
     });
 }
+
+
