ab9adb9b12f4b31e45506ff0a8848fb688398279
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-10-06 22:42:54 +0900

    Preloader test for ios

--- src/global/preloader.js
+++ src/global/preloader.js
@@ -1,10 +1,5 @@
 const preloader = document.querySelector('#preloader');
 
 window.addEventListener('load', function() {
-    preloader.classList.add('preloader__hiding');
-    
-    preloader.addEventListener('transitionend', function() {
-        this.classList.add('preloader__hidden');
-        this.classList.remove('preloader__hiding');
-    });
+    preloader.classList.add('preloader__hidden');
 });
