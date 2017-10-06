91d716b0d04b2631b99ae61959a0eb4cf9e9bf86
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-10-06 23:08:22 +0900

    Change const to var in preloader.js

--- src/global/preloader.js
+++ src/global/preloader.js
@@ -1,5 +1,10 @@
-const preloader = document.querySelector('#preloader');
+var preloader = document.querySelector('#preloader');
 
 window.addEventListener('load', function() {
-    preloader.style.background = "blue";
-}, false);
+    preloader.classList.add('preloader__hiding');
+    
+    preloader.addEventListener('transitionend', function() {
+        this.classList.add('preloader__hidden');
+        this.classList.remove('preloader__hiding');
+    });
+});
