38b9a1eda2b12b54ebd76a0dc766abfd60a3ae08
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-10-06 23:10:15 +0900

    Use const inside function in preloader.js

--- src/global/preloader.js
+++ src/global/preloader.js
@@ -1,6 +1,5 @@
-var preloader = document.querySelector('#preloader');
-
 window.addEventListener('load', function() {
+    const preloader = document.querySelector('#preloader');
     preloader.classList.add('preloader__hiding');
     
     preloader.addEventListener('transitionend', function() {
