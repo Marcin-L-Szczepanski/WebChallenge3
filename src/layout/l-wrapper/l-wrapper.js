a829627f145d6c0efb192b2f1c9ca791790cf304
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-11-03 08:21:23 +0900

    Test fade-in

--- src/layout/l-wrapper/l-wrapper.js
+++ src/layout/l-wrapper/l-wrapper.js
@@ -5,6 +5,7 @@ document.addEventListener("scroll", function () {
   let i = 1;
   
   for(i; wrapper.length; i++) {
+    if(i === 2) { continue; }
     let wrapperOffsetTop = wrapper[i].offsetTop;
     if(scrollPosition > (wrapperOffsetTop - 500)) {
       wrapper[i].classList.add("l-wrapper--showing");
