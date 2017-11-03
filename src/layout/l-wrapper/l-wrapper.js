cc9609b337dee0e4955c7d01a694e9f30597ee39
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-11-03 09:15:11 +0900

    Start animation earlier

--- src/layout/l-wrapper/l-wrapper.js
+++ src/layout/l-wrapper/l-wrapper.js
@@ -7,7 +7,7 @@ document.addEventListener("scroll", function () {
   for(i; wrapper.length; i++) {
     if(i === 2) { continue; }
     let wrapperOffsetTop = wrapper[i].offsetTop;
-    if(scrollPosition > (wrapperOffsetTop - (document.documentElement.clientHeight * 0.7))) {
+    if(scrollPosition > (wrapperOffsetTop - (document.documentElement.clientHeight * 0.8))) {
       wrapper[i].classList.add("l-wrapper--showing");
     }
   }
