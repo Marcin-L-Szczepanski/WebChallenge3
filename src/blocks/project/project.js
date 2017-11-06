785fbe39332d6cbc4c4b0d2ee797e5803eef209c
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2017-11-07 07:51:20 +0900

    Fix animation for IE and Edge

--- src/blocks/project/project.js
+++ src/blocks/project/project.js
@@ -1,4 +1,5 @@
 function fadeIn(element) {
+  "use strict";
   element.style.opacity = 1;
   element.style.transform = "translateY(0px)";
 }
@@ -6,12 +7,12 @@ function fadeIn(element) {
 document.addEventListener("scroll", function () {
   "use strict";
   const project = document.querySelector(".project");
-  const projectPosition = project.getBoundingClientRect().y;
+  const projectPosition = project.getBoundingClientRect().top;
   const screenHeight = document.documentElement.clientHeight;
   const feature = document.querySelectorAll(".feature--inline");
   let i = 0;
   
-  if(projectPosition / screenHeight < 0.75) {
+  if((projectPosition / screenHeight) < 0.75) {
     setInterval(function () {
       if(i < feature.length) {
         feature[i].classList.add("feature--showing");
